beda54385935c14f8d579d292601ef7d8c7c7446
Will Sergeant <crgk71@gmail.com>
2016-09-17 23:03:22 -0400

    tried to setup vscode on mac, also wrote hw.cs

---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //A simple program to display the words hello world
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
