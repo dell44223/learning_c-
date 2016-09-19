d559220295ce39a455ae4b46bf8ab8526376db6e
Will Sergeant <crgk71@hotmail.com>
2016-09-18 22:54:12 -0400

    Chapter 5 of C# in 24hrs completed.

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -10,8 +10,19 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Read();
+            //Console.WriteLine("Hello World");
+            //Console.Read();
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            string userAge = Console.ReadLine();
+            Console.WriteLine("Please enter the current year: ");
+            string currentYear = Console.ReadLine();
+            int yearBorn = Convert.ToInt32(currentYear);
+            int userAge2 = Convert.ToInt32(userAge);
+            yearBorn = yearBorn - userAge2;
+            Console.WriteLine("Hello World! My name is " + userName + " and I am " + userAge + " years old. I was born in " + yearBorn + ".");
+            Console.ReadLine();
         }
     }
 }
